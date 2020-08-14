c61137d1dfdf363e36e6b96f8d71c5d9a24d036c
Alan Nascimento <alan.silva.n@hotmail.com>
2020-08-14 14:25:03 -0300

    chore: setup project

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import { CrossStorageClient } from 'cross-storage'
+
+function App() {
+  const storage = new CrossStorageClient('https://cross-domain-server.herokuapp.com/');
+
+  storage.onConnect()
+    .then(() => storage.set('key', JSON.stringify({ foo: 'bar' })))
+    .then(() => console.warn(storage._hub.Storage))
+    .catch(console.error)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
